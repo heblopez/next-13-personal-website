f0d324ab5adf32b7b9ae783b218ec74688ad7534
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-03 19:09:01 -0500

    Added more styles to individual project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -8,6 +8,15 @@ export default async function Project({params}: Props) {
   const slug = params.project;
   const project = await getProject(slug);
   return (
-    <h1>{project.name}</h1>
+    <div className="max-w-3xl mx-auto py-20 px-8">
+      <header className="flex items-center justify-between">
+        <h1 className="text-5xl drop-shadow font-extrabold bg-gradient-to-l from-orange-400 via-red-500 to-purple-700 bg-clip-text text-transparent">{project.name}</h1>
+        <a href={project.url} title="View project" target="_blank" rel="noopener noreferrer" className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-purple-500 hover:text-purple-50 transition"> View Project</a>
+      </header>
+
+      {/* Content goes here */}
+
+      {/* Image goes here */}
+    </div>
   )
 }
