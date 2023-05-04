3ebf42b765fa40fffdf1a9e35ef7e1975f3da102
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-03 19:24:46 -0500

    Added content and image with styles to individual project page

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default async function Home() {
   return (
     <div className="max-w-5xl mx-auto py-20 px-8">
       <h1 className="text-7xl text-center font-extrabold"> Hi, I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-700 bg-clip-text text-transparent">Heberth Lopez</span>!</h1>
-      <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my projects:</p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <p className="mt-8 text-xl text-gray-600">Aloha everyone! Check out my projects:</p>
+      <h2 className="mt-16 font-bold text-gray-700 text-3xl">My Projects</h2>
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       { projects.map(project => (
         <Link href={`/projects/${project.slug}`} key={project._id} className="border-2 border-gray-500 rounded-lg p-3 hover:scale-105 hover:border-purple-700 transition">
           { project.image && (
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from "@/sanity/sanity.utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string }
@@ -8,15 +10,19 @@ export default async function Project({params}: Props) {
   const slug = params.project;
   const project = await getProject(slug);
   return (
-    <div className="max-w-3xl mx-auto py-20 px-8">
+    <div className="max-w-5xl mx-auto py-20 px-8">
       <header className="flex items-center justify-between">
         <h1 className="text-5xl drop-shadow font-extrabold bg-gradient-to-l from-orange-400 via-red-500 to-purple-700 bg-clip-text text-transparent">{project.name}</h1>
         <a href={project.url} title="View project" target="_blank" rel="noopener noreferrer" className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-purple-500 hover:text-purple-50 transition"> View Project</a>
       </header>
 
       {/* Content goes here */}
+      <div className="text-lg text-gray-700 my-8">
+        <PortableText value={project.content}/>
+      </div>
 
       {/* Image goes here */}
+      <Image src={project.image} alt={project.name} width={1920} height={1080} className="border-2 border-purple-700 object-cover rounded-xl"/>
     </div>
   )
 }
