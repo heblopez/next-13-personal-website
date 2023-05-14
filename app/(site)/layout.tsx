89079a9cd5ac661ccb1bfc85175537e65881ff06
Heberth Lopez <heberth.lopez.19@gmail.com>
2023-05-14 18:10:20 -0500

    Reorganization folders in NextJS

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import "./globals.css"
+import "../globals.css"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className}`} >
-        <header className="bg-white px-4 py-5 top-0 sticky">
+        <header className="max-w-5xl mx-auto bg-white px-4 py-5 top-0 sticky">
           <Link className="bg-gradient-to-l from-orange-400 via-red-500 to-purple-700 bg-clip-text text-transparent text-2xl drop-shadow font-extrabold p-4" href={'/'}> @heblopez </Link>
         </header>
         <main className="max-w-5xl mx-auto px-8 py-16">{children}</main>
